f80a7d2224388f2f596793562fd3d346724d6cca
mantharey <Samantha@Macintosh.local>
2016-08-01 21:58:13 -0400

    Fixed image

--- scripts/album.js
+++ scripts/album.js
@@ -4,7 +4,7 @@ var albumPicasso = {
     artist: 'Pablo Picasso',
     label: 'Cubism',
     year: '1881',
-    albumArtURL: 'assets/images/album_covers/01.png',
+    albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
         { title: 'Blue', duration: '4:26' },
          { title: 'Green', duration: '3:14' },
